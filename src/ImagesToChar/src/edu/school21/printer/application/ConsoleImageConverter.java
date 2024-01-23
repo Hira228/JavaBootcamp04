7ed765b80e814a84b591972094d16eb66a99cf5a
Grady Zankale <gradyzan@at-g1.msk.21-school.ru>
2024-01-23 22:38:48 +0300

    sdfasdf

---
+++ src/ImagesToChar/src/edu/school21/printer/application/ConsoleImageConverter.java
@@ -0,0 +1,9 @@
+package edu.school21.printer.application;
+import edu.school21.printer.logic.Logic;
+
+public class ConsoleImageConverter {
+    public static void main(String[] args) {
+        Logic logic = new Logic();
+        logic.printImage();
+    }
+}
---
+++ src/ImagesToChar/src/edu/school21/printer/logic/Logic.java
@@ -0,0 +1,34 @@
+package edu.school21.printer.logic;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Logic {
+    public void printImage() {
+        byte[][] result;
+        try {
+            result = seeBMPImage();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        for (int y = 0; y < result[0].length; y++) {
+            for (byte[] ints : result) {
+                System.out.print(ints[y] < 0 ? "." : "0");
+            }
+            System.out.println();
+        }
+    }
+    public byte[][] seeBMPImage() throws IOException {
+        File image = new File("/Users/gradyzan/IdeaProjects/ImagesToChar/src/it.bmp");
+        BufferedImage bufferedImage = ImageIO.read(image);
+        byte[][] arr = new byte[bufferedImage.getWidth()][bufferedImage.getHeight()];
+        for(int i = 0; i < bufferedImage.getWidth(); ++i) {
+            for(int j = 0; j < bufferedImage.getHeight(); ++j) {
+                arr[i][j] = (byte)(bufferedImage.getRGB(i, j) >> 8) ;
+            }
+        }
+        return arr;
+    }
+}
