d081a7805e9619d5ec3dbf35704586686e943786
Grady Zankale <gradyzan@at-g1.msk.21-school.ru>
2024-01-24 20:16:21 +0300

    sdfasdfasdf

--- src/ex02/ImagesToChar/README.txt
+++ src/ex02/ImagesToChar/README.txt
@@ -1,6 +1,9 @@
 mkdir target
 mkdir target/resource
 cp src/resource/it.bmp target/resource
-javac -cp src/lib/JCDP-4.0.2.jar:src/lib/jcommander-1.82.jar src/java/edu/school21/printer/*/*.java -d ./target
+javac -cp ./src/lib/JCDP-4.0.2.jar:./src/lib/jcommander-1.82.jar src/java/edu/school21/printer/*/*.java -d ./target
+cd target/
+jar xf ../src/lib/JCDP-4.0.2.jar && jar xf ../src/lib/jcommander-1.82.jar
+cd ../
 jar cvmf src/manifest.txt target/images-to-chars-printer.jar target/edu/school21/printer/application/ConsoleImageConverter.class
-java -jar target/images-to-chars-printer.jar
+java -jar target/images-to-chars-printer.jar --white=black --black=green
--- src/ex02/ImagesToChar/src/java/edu/school21/printer/logic/Logic.java
+++ src/ex02/ImagesToChar/src/java/edu/school21/printer/logic/Logic.java
@@ -10,31 +10,47 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 @Parameters(separators = "=")
 class argsString {
+    Map<String, Ansi.BColor> colors;
+    argsString(){
+        colors = new HashMap<>();
+        colors.put("RED", Ansi.BColor.RED);
+        colors.put("BLACK", Ansi.BColor.BLACK);
+        colors.put("GREEN", Ansi.BColor.GREEN);
+        colors.put("BLUE", Ansi.BColor.BLUE);
+        colors.put("YELLOW", Ansi.BColor.YELLOW);
+        colors.put("CYAN", Ansi.BColor.CYAN);
+        colors.put("MAGENTA", Ansi.BColor.MAGENTA);
+        colors.put("NONE", Ansi.BColor.NONE);
+    }
     @Parameter(
             names = { "--white"},
             arity = 1,
             required = true
     )
-    public  String mama;
+    private  String white;
 
     @Parameter(
             names = { "--black"},
             arity = 1,
             required = true
     )
-    public String kaka;
+    private String black;
 
-     Map<String, String> colors = new HashMap<>();
-
-
-    public Map<String, String> getColors() {
+    public Map<String, Ansi.BColor> getColors() {
         return colors;
+    }
+
+    public String getWhite() {
+        return white;
+    }
+
+    public String getBlack() {
+        return black;
     }
 }
 
@@ -42,41 +58,29 @@ public class Logic {
     String[] args;
     public Logic(String[] args) {
         this.args = args;
-    };
+    }
     public void printImage() {
         argsString as = new argsString();
         JCommander jCommander = JCommander.newBuilder().addObject(as).build();
+        jCommander.setCaseSensitiveOptions(false);
         jCommander.parse(args);
-        System.out.println(as.kaka + " " + as.mama);
-        Map<String, String> colors = as.getColors();
-        for (Map.Entry<String, String> entry : colors.entrySet()) {
-            System.out.println("Color " + entry.getKey() + ": " + entry.getValue());
-        }
+        Map<String, Ansi.BColor> colors = as.getColors();
         ColoredPrinter cp = new ColoredPrinter.Builder(1, false).build();
+
         byte[][] result;
         try {
-            result = seeBMPImage();
+              result = seeBMPImage();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
         for (int y = 0; y < result[0].length; y++) {
             for (byte[] ints : result) {
-                //System.out.println(ints[y]);
-                if(ints[y] < 0) cp.setBackgroundColor(Ansi.BColor.RED);
-                else cp.setBackgroundColor(Ansi.BColor.GREEN);
-                cp.print("   ");
+                    if(ints[y] < 0) cp.setBackgroundColor(colors.get(as.getWhite().toUpperCase()));
+                    else cp.setBackgroundColor(colors.get(as.getBlack().toUpperCase()));
+                    cp.print("   ");
             }
             System.out.println();
         }
-
-        for (int y = 0; y < result[0].length; y++) {
-            for (byte[] ints : result) {
-                System.out.print(ints[y] < 0 ? "." : "0");
-            }
-            System.out.println();
-        }
-
-
     }
     public byte[][] seeBMPImage() throws IOException {
         File image = new File("./target/resource/it.bmp");
